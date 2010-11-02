4fcd9d9165733d7f6242478f7bedf18166bac220
Open2C ERP <openc.erp@gmail.com>
2010-11-02 22:14:36 +0300

    first commit

---
+++ ZipArchive/ZipBigFile.cpp
@@ -0,0 +1,42 @@
+// ZipBigFile.cpp: implementation of the CZipBigFile class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "ZipBigFile.h"
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+IMPLEMENT_DYNAMIC(CZipBigFile, CFile)
+
+CZipBigFile::CZipBigFile()
+{
+
+}
+
+CZipBigFile::~CZipBigFile()
+{
+
+}
+
+ULONGLONG CZipBigFile::Seek(LONGLONG dOff, UINT nFrom)
+//_int64 CZipBigFile::Seek(_int64 dOff, UINT nFrom) // 23.10.2010
+{
+	ASSERT_VALID(this);
+	ASSERT(m_hFile != (UINT)hFileNull);
+	ASSERT(nFrom == begin || nFrom == end || nFrom == current);
+	ASSERT(begin == FILE_BEGIN && end == FILE_END && current == FILE_CURRENT);
+	LARGE_INTEGER li;
+	li.QuadPart = dOff;
+
+	li.LowPart  = ::SetFilePointer((HANDLE)m_hFile, li.LowPart, &li.HighPart, (DWORD)nFrom);
+	DWORD dw = GetLastError();
+	if ((li.LowPart == (DWORD)-1) && (dw != NO_ERROR))
+	{
+		CFileException::ThrowOsError((LONG)dw);
+	}
+
+	return li.QuadPart;
+
+}
